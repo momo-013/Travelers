6b7f354410196312dd8edb574752986bd0dffe4f
momo-013 <hayatana1116@gmail.com>
2023-04-19 18:06:40 +0900

    migration

---
+++ database/migrations/2023_04_19_133925_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('place');
+            $table->integer('star');
+            $table->string('title', 20);
+            $table->string('body', 200);
+            $table->string('image_url')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('prefecture_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
